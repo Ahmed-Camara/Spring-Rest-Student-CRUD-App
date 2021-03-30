4172b230964a93eadfbaa573142ee14d2c3e2da3
Ahmed-Camara <camarahmed123@gmail.com>
2021-03-30 11:17:32 +0600

    update student with PUT mapping

--- src/main/java/com/srm/rest/controller/StudentRestController.java
+++ src/main/java/com/srm/rest/controller/StudentRestController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.srm.rest.entity.Student;
 import com.srm.rest.exceptionsHandler.StudentNotFoundException;
 import com.srm.rest.service.StudentService;
@@ -45,12 +47,20 @@ public class StudentRestController {
 	@PostMapping("/student")
 	public Student addStudent(@RequestBody Student student) {
 		
-		System.out.println("======");
+		
 		student.setId(0);
 		
 		studentService.saveStudent(student);
 		
 		System.out.println(student);
+		
+		return student;
+	}
+	
+	@PutMapping("/student")
+	public Student updateStudent(@RequestBody Student student) {
+		
+		studentService.saveStudent(student);
 		
 		return student;
 	}
