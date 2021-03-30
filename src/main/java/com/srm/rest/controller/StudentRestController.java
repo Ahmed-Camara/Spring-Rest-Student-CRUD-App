2ff68dde6fae0f3b9039cc507d5f3fe1b114a63f
Ahmed-Camara <camarahmed123@gmail.com>
2021-03-30 11:10:21 +0600

    add student with POST request

--- src/main/java/com/srm/rest/controller/StudentRestController.java
+++ src/main/java/com/srm/rest/controller/StudentRestController.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.srm.rest.entity.Student;
 import com.srm.rest.exceptionsHandler.StudentNotFoundException;
 import com.srm.rest.service.StudentService;
@@ -37,6 +38,19 @@ public class StudentRestController {
 			throw new StudentNotFoundException("Student not found id : "+studentID);
 		}
 		
+		
+		return student;
+	}
+	
+	@PostMapping("/student")
+	public Student addStudent(@RequestBody Student student) {
+		
+		System.out.println("======");
+		student.setId(0);
+		
+		studentService.saveStudent(student);
+		
+		System.out.println(student);
 		
 		return student;
 	}
