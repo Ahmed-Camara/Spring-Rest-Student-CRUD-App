c70592b59b627a829b1ccf40cff15b26083986ed
Ahmed-Camara <camarahmed123@gmail.com>
2021-03-30 08:55:01 +0600

    Spring rest api starting files

---
+++ src/main/java/com/srm/rest/service/StudentServiceImpl.java
@@ -0,0 +1,39 @@
+package com.srm.rest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.srm.rest.dao.StudentDAO;
+import com.srm.rest.entity.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	@Autowired
+	private StudentDAO studentDAO;
+	
+	@Override
+	public List<Student> getStudents() {
+		
+		return studentDAO.getStudents();
+	}
+
+	@Override
+	public void saveStudent(Student student) {
+		
+		studentDAO.saveStudent(student);
+	}
+	
+	@Override
+	public void deleteStudent(int id) {
+		studentDAO.deleteStudent(id);
+	}
+	
+	@Override
+	public Student getStudent(int id) {
+		
+		return studentDAO.getStudent(id);
+	}
+}
